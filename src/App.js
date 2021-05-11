a827f7a81407515e2b0f24dd31a81bda6a2e99d7
Edouard Misset <edouardmisset@gmail.com>
2021-05-11 11:53:39 +0200

    Added the particles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+        value_area: 3500,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 100,
+      opacity: 0.1,
+    },
+    move: {
+      direction: 'right',
+      speed: 0.2,
+    },
+    size: {
+      value: 1,
+    },
+    opacity: {
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.05,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onclick: {
+        enable: true,
+        mode: 'push',
+      },
+    },
+    modes: {
+      push: {
+        particles_nb: 1,
+      },
+    },
+  },
+  retina_detect: true,
+};
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Particles params={particlesOptions} className='particles' />
+    </div>
+  );
 }
 
 export default App;
