7fb1980895769b1bf2b95f42e6637dcfeb138bc9
Edouard Misset <edouardmisset@gmail.com>
2021-05-11 23:54:10 +0200

    bug fixed when ship moved passed the sides

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -4,13 +4,13 @@ import './Ship.css';
 function convertDegToRad(deg) {
   return (Math.PI * deg) / 180;
 }
+// Speed factor
 const speed = 10;
 function Ship({ ship, setShip }) {
   const { angle, position } = ship;
   const { x, y } = position;
 
   function moveShip(e) {
-    // Speed factor
 
     switch (e.key) {
       case 'ArrowLeft':
@@ -21,15 +21,20 @@ function Ship({ ship, setShip }) {
         break;
       case 'ArrowUp':
         // redefinir position
+        
         setShip(ship => ({
           ...ship,
           position: {
             x:
-              ship.position.x +
-              speed * Math.cos(speed * convertDegToRad(ship.angle)),
+              ship.position.x <= 0
+                ? (ship.position.x = 1)
+                : ship.position.x +
+                  speed * Math.cos(speed * convertDegToRad(ship.angle)),
             y:
-              ship.position.y +
-              speed * Math.sin(speed * convertDegToRad(ship.angle)),
+              ship.position.y <= 0
+                ? (ship.position.y = 1)
+                : ship.position.y +
+                  speed * Math.sin(speed * convertDegToRad(ship.angle)),
           },
         }));
         break;
@@ -37,7 +42,7 @@ function Ship({ ship, setShip }) {
         console.log('key pressed');
     }
   }
-
+  console.log(position);
   useEffect(() => {
     const rocket = document.querySelector('.rocket');
     rocket.style.transform = `rotate(${90 + speed * angle}deg)`;
