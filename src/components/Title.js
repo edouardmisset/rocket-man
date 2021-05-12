03b11761e1c4f254c82a7e9af8fe6af40be47e41
Edouard Misset <edouardmisset@gmail.com>
2021-05-12 13:02:27 +0200

    Title works :D

--- src/components/Title.js
+++ src/components/Title.js
@@ -11,22 +11,20 @@ export default function Title() {
     setTrack(y => ({ ...track, y: (4 * mouseY) / 570 + 50 }));
   }
 
-  // useEffect(() => {
-  //   document.body.addEventListener('mouseover', mouseFollow);
-  //   return () => {
-  //     document.body.removeEventListener('mouseover', mouseFollow);
-  //   };
-  // }, []);
+  useEffect(() => {
+    document.body.addEventListener('mousemove', mouseFollow);
+    return () => {
+      document.body.removeEventListener('mousemove', mouseFollow);
+    };
+  }, []);
 
   return (
-    <div>
-      <div className='container' onMouseOver={e => mouseFollow(e)}>
-        <div
-          className='title'
-          style={{ backgroundPosition: `${track.x}% ${track.y}%` }}
-        >
-          Team Rocket
-        </div>
+    <div className='container'>
+      <div
+        className='title'
+        style={{ backgroundPosition: `${track.x}% ${track.y}%` }}
+      >
+        Team Rocket
       </div>
     </div>
   );
